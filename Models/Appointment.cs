51ced7960b4f29c9ab950ee2ef43992f99930133
huiglotalkin-cpu <huiglotalkin@gmail.com>
2026-03-17 14:21:30 +0600

    Add Appointment model

---
+++ Models/Appointment.cs
@@ -0,0 +1,8 @@
+// Placeholder class for Appointment Model
+public class Appointment
+{
+    public int Id { get; set; }
+    public DateTime AppointmentDate { get; set; }
+    public int PatientId { get; set; }
+    public int DoctorId { get; set; }
+}
